5e4112a55e182cf231aa2009ef98884501906acb
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-17 06:47:06 +0000

    Automatically backed up by Learn

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,7 +16,6 @@ class Game extends React.Component {
     };
   }
 
-  // TODO: create a componentDidMount() which will set the current time
   componentDidMount() {
     return setCurrentTime()
   }
